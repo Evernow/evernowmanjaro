2e4239235d3c5c7b360145db223fd5714d357a91
Evernow <44484725+Evernow@users.noreply.github.com>
2022-06-29 01:20:29 -0400

    Handle ddrescue-gui dependencies

--- BuildAURPackages/BuildAURPackages.py
+++ BuildAURPackages/BuildAURPackages.py
@@ -1,14 +1,21 @@
 import subprocess,shutil,glob,os
 
 # {'Package name': ['Package location if AUR, if not then doesn't matter', 'command to launch']}
-packages_to_add = {'ddrescue-gui' : ['AUR','python3 /usr/share/ddrescue-gui/DDRescue_GUI.py'],
+packages_to_add = {
                     'mprime-bin': ['AUR', 'mprime'],
                     'gputest': ['AUR', None],
                     'nvflash': ['AUR', None],
-                    'python-getdevinfo': ['AUR', None]}
+                    'ddrescue' : ['extra', None], # Dependency of ddrescue-gui
+                    'lshw' : ['extra', None], # Dependency of ddrescue-gui
+                    'python-beautifulsoup4' : ['extra', None], # Dependency of ddrescue-gui
+                    'parted' : ['extra', None], # Dependency of ddrescue-gui
+                    'python-getdevinfo': ['AUR', None], # Dependency of ddrescue-gui
+                    'python-wxpython': ['AUR', None], # Dependency of ddrescue-gui
+                    'ddrescue-gui' : ['AUR','python3 /usr/share/ddrescue-gui/DDRescue_GUI.py']}
 
 def buildAURPackages(AURPACKAGES):
     subprocess.run('sudo pacman -S makepkg --noconfirm',shell=True)
+    subprocess.run('mkdir online-repo',shell=True)
     subprocess.run('mkdir online-repo/x86_64',shell=True)
     os.makedirs('/AURPackagesToRepo')
     for package in AURPACKAGES:
@@ -31,7 +38,12 @@ subprocess.run('git clone https://gitlab.manjaro.org/profiles-and-settings/iso-p
 
 
 AURPACKAGES = []
+repopackages = []
 for package in packages_to_add:
     if packages_to_add[package][0] == 'AUR':
         AURPACKAGES.append(package)
+    else:
+        repopackages.append(package)
+for package in repopackages:
+    subprocess.run('pacman -S {package} --noconfirm'.format(package=package),shell=True)
 buildAURPackages(AURPACKAGES)
