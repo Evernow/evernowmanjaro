216b86862d080423a93237757dfe45eeafcbc229
Evernow <44484725+Evernow@users.noreply.github.com>
2022-07-01 05:18:19 -0400

    makepkg -si

--- BuildAURPackages/BuildAURPackages.py
+++ BuildAURPackages/BuildAURPackages.py
@@ -24,7 +24,7 @@ def buildAURPackages(AURPACKAGES):
         subprocess.run('git clone https://aur.archlinux.org/{package}.git'.format(package=package),shell=True)
       #  subprocess.run('chmod 777 /{package}'.format(package=package),shell=True)
       #  subprocess.run('sudo -u nobody ls',shell=True,cwd='/{package}'.format(package=package))
-        subprocess.run('makepkg si --skipinteg --skipchecksums --skippgpcheck',shell=True,cwd='/{package}'.format(package=package))
+        subprocess.run('makepkg -si --skipinteg --skipchecksums --skippgpcheck',shell=True,cwd='/{package}'.format(package=package))
       #  subprocess.run('pacman -U {package} --noconfirm'.format(package=glob.glob('/{package}/*.zst'.format(package=package))[0]),shell=True)
         print('Command that is maybe failing here')
         shutil.copy(glob.glob('/{package}/*.zst'.format(package=package))[0], '/AURPackagesToRepo{nameofbuild}'.format(nameofbuild=glob.glob('/{package}/*.zst'.format(package=package))[0][glob.glob('/{package}/*.zst'.format(package=package))[0].rfind('/'):]))
