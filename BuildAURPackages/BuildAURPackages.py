cf8c785da8798e49eb947e7512e8f9293e2399b4
Evernow <44484725+Evernow@users.noreply.github.com>
2022-07-03 06:42:53 -0400

    Update file extensions (Arch switched)

--- BuildAURPackages/BuildAURPackages.py
+++ BuildAURPackages/BuildAURPackages.py
@@ -35,7 +35,7 @@ def buildAURPackages(AURPACKAGES):
   
     for f in allfiles:
         shutil.move('/AURPackagesToRepo/' + f, '/online-repo/x86_64/' + f)
-    subprocess.run('repo-add /online-repo/x86_64/online-repo.db.tar.gz *.pkg.tar.gz',shell=True,cwd='/{repoaddress}'.format(repoaddress='/online-repo/x86_64'))
+    subprocess.run('repo-add /online-repo/x86_64/online-repo.db.tar.gz *.pkg.tar.zst',shell=True,cwd='/{repoaddress}'.format(repoaddress='/online-repo/x86_64'))
 
 
 def HandleAURdependenciesFromAUR(package):
