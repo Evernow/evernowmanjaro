ca1976d50e3c4358ed0ab91e906631629d8f37f1
Evernow <44484725+Evernow@users.noreply.github.com>
2022-07-06 09:10:03 -0400

    Correct cp command

--- BuildISO.py
+++ BuildISO.py
@@ -14,7 +14,7 @@ subprocess.run('git clone https://gitlab.manjaro.org/profiles-and-settings/iso-p
 def SetupPhoronixSuite():
     subprocess.run('pacman -S phoronix-test-suite --noconfirm',shell=True,check=True)
     subprocess.run('phoronix-test-suite make-download-cache pts/ddnet pts/realsr-ncnn pts/octanebench pts/gravitymark',shell=True,check=True)
-    subprocess.run('cp /var/cache/phoronix-test-suite/download-cache ISO-Components/etc/Desktop/Configs/',shell=True,check=True)
+    subprocess.run('cp -r /var/lib/phoronix-test-suite/download-cache ISO-Components/etc/Desktop/Configs/',shell=True,check=True)
 
 def cleanuppackages():
     listofpackagefiles = ['Packages-Desktop', 'Packages-Live']
