ea2e7208bdec07f301f836cb20afecef29c8c189
Evernow <44484725+Evernow@users.noreply.github.com>
2022-08-29 05:55:43 -0400

    Fix gravitymark

---
+++ ISO-Components/etc/.config/plasma-workplace/env/gravitymark.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ 
+chmod +x ~/Desktop/Configs/phoronix-test-suite/download-cache/GravityMark_1.53.run
+echo "y" | ~/Desktop/Configs/phoronix-test-suite/download-cache/GravityMark_1.53.run --quiet --target gravity-install --nox11 --noexec
+ 
+echo "#!/bin/sh
+cd gravity-install/bin
+LD_LIBRARY_PATH=.:\$LD_LIBRARY_PATH ./GravityMark.x64 -vsync 0 -fps 1 -benchmark 1 -close 1 -fullscreen 1 -times 100 \$@ > \$LOG_FILE 2>&1" > gravitymark
+chmod +x gravitymark
